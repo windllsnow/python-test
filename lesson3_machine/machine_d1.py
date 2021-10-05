81615ac3c8f615dd6ba1651bcd1ae49eda8e2b5c
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-05 21:05:43 +0800

    Add files via upload

---
+++ lesson3_machine/machine_d1.py
@@ -0,0 +1,22 @@
+#%%
+
+#預載  
+
+# conda update --all
+# conda install -c conda-forge tesseract
+# conda install -c conda-forge pytesseract
+# conda install -c conda-forge imutils
+# conda install opencv
+# conda install tensorflow
+
+
+#%%
+
+
+
+
+#%%
+
+
+
+#%%
