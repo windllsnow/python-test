fb93fbbd729afc31818837b1be3a0af0ae0e8fa9
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-14 01:54:32 +0800

    Add files via upload

---
+++ project01/areapackage/myClass.py
@@ -0,0 +1,11 @@
+
+class Rectangle():
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+    def area1(self):
+        return self.width*self.height
+
+class Triangle(Rectangle):
+    def area2(self):
+        return (self.width*self.height)/2
