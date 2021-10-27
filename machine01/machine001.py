9e1bf16ef5b82e904059f24efa577137ee454918
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-27 23:40:29 +0800

    Add files via upload

---
+++ machine01/machine001.py
@@ -0,0 +1,52 @@
+#%%
+
+
+
+
+
+# %%
+
+# %%
+
+# %%
+#2-1 微積分
+import matplotlib.pyplot as plt
+alchol=58
+for x in range(0,11):
+    if x>0:
+        alchol*=0.5
+    print(f"當x={x:2d},酒精濃度={alchol}")
+
+
+
+# %%
+import matplotlib.pyplot as plt
+alchol=58
+x=[x for x in range(0,11)]
+y=[alchol*(1/2)**y for y in x]
+plt.axis([0,12,0,60])
+plt.plot(x,y)
+plt.xlabel("Times")
+plt.ylabel("Alcohol concentration")
+plt.grid()
+plt.show()
+# %%
+import matplotlib.pyplot as plt
+x=[x for x in range(1,101)]
+y=[1/y for y in x]
+plt.axis([0,100,0,2])
+plt.plot(x,y)
+plt.xlabel("n")
+plt.ylabel("y")
+plt.grid()
+plt.show()
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
