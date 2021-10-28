da98396b8abbfc50ed45f6a9d852985d17849163
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-28 23:37:13 +0800

    Add files via upload

--- machine01/machine001.py
+++ machine01/machine001.py
@@ -42,7 +42,17 @@ plt.grid()
 plt.show()
 
 # %%
-
+import  matplotlib.pyplot as plt 
+import numpy as np
+x=np.linspace(1,0.01,101)
+y=[1/y for y in x]
+plt.axis([0,1,0,101])
+plt.plot(x,y)
+plt.plot(x[100],y[100],'-o')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.show()
 # %%
 
 # %%
