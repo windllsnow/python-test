947815a86b269fa6377103ae10c3e0a400fc7edd
windllsnow <87889501+windllsnow@users.noreply.github.com>
2021-10-30 22:23:03 +0800

    Add files via upload

--- tkinter001/tktk.py
+++ tkinter001/tktk.py
@@ -1088,97 +1088,275 @@ root.mainloop()
 
 
 # %%
+# 標籤 和文字方塊
 
+from tkinter import*
+root=Tk()
+root.title("ch5_1")
+namel=Label(root,text="Name")
 
+namel.grid(row=0)
+addressl=Label(root,text="Address")
+addressl.grid(row=1)
 
+nameE=Entry(root)
+addressE=Entry(root)
+
+nameE.grid(row=0,column=1)
+addressE.grid(row=1,column=1)
+root.mainloop()
+root.destory()
 # %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-# %%
-
-
-
-
-# %%
-
-
-
-
-
 from tkinter import *
 
 root = Tk()
-app = Frame(root)
-hello = Label(root, text="Hello Tk!", width="30", height="5")
-hello.pack()
+root.title("ch5_2")                     # 視窗標題
+
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=0)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=1)
+
+accountE=Entry(root)
+pwdE=Entry(root,show="*")   # key 進去 ，顯示都是*
+accountE.grid(row=0,column=1)
+pwdE.grid(row=1,column=1)
+
 root.mainloop()
+root.destroy()
+
+# %%
+from tkinter import *
+
+root = Tk()
+root.title("ch5_3")                     # 視窗標題
+
+msg = "歡迎進入Silicon Stone Educaiton系統"
+sseGif = PhotoImage(file="sse.gif")     # Logo影像檔
+logo = Label(root,image=sseGif,text=msg,compound=BOTTOM)
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=1)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=2)
+
+logo.grid(row=0,column=0,columnspan=2,pady=10,padx=10)
+accountE = Entry(root)                  # 文字方塊account
+pwdE = Entry(root,show="*")             # 文字方塊pwd
+accountE.grid(row=1,column=1)           # 定位文字方塊account
+pwdE.grid(row=2,column=1,pady=10)       # 定位文字方塊pwd
+
+root.mainloop()
+
+
+
+
+
+# %%
+from tkinter import *
+def printInfo():                        # 列印輸入資訊
+    print("Account: %s\nPassword: %s" % (accountE.get(),pwdE.get()))
+    
+root = Tk()
+root.title("ch5_4")                     # 視窗標題
+
+msg = "歡迎進入Silicon Stone Educaiton系統"
+sseGif = PhotoImage(file="sse.gif")     # Logo影像檔
+logo = Label(root,image=sseGif,text=msg,compound=BOTTOM)
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=1)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=2)
+
+logo.grid(row=0,column=0,columnspan=2,pady=10,padx=10)
+accountE = Entry(root)                  # 文字方塊account
+pwdE = Entry(root,show="*")             # 文字方塊pwd
+accountE.grid(row=1,column=1)           # 定位文字方塊account
+pwdE.grid(row=2,column=1,pady=10)       # 定位文字方塊pwd
+# 以下建立Login和Quit案鈕
+loginbtn = Button(root,text="Login",command=printInfo)
+loginbtn.grid(row=3,column=0)
+quitbtn = Button(root,text="Quit",command=root.destroy)
+quitbtn.grid(row=3,column=1)
+
+root.mainloop()
+
+#%%
+from tkinter import *
+def printInfo():                        # 列印輸入資訊
+    print("Account: %s\nPassword: %s" % (accountE.get(),pwdE.get()))
+    
+root = Tk()
+root.title("ch5_5")                     # 視窗標題
+
+msg = "歡迎進入Silicon Stone Educaiton系統"
+sseGif = PhotoImage(file="sse.gif")     # Logo影像檔
+logo = Label(root,image=sseGif,text=msg,compound=BOTTOM)
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=1)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=2)
+
+logo.grid(row=0,column=0,columnspan=2,pady=10,padx=10)
+accountE = Entry(root)                  # 文字方塊account
+pwdE = Entry(root,show="*")             # 文字方塊pwd
+accountE.grid(row=1,column=1)           # 定位文字方塊accou
+pwdE.grid(row=2,column=1,pady=10)       # 定位文字方塊pwd
+# 以下建立Login和Quit案鈕
+loginbtn = Button(root,text="Login",command=printInfo)
+loginbtn.grid(row=3,column=0,sticky=W,pady=5)   # 切齊
+quitbtn = Button(root,text="Quit",command=root.destroy)
+quitbtn.grid(row=3,column=1,sticky=W,pady=5)   # 切齊
+
+root.mainloop()
+
+
+
+
+
+
+# %%
+
+from tkinter import *
+def printInfo():                        # 列印輸入資訊
+    print("Account: %s\nPassword: %s" % (accountE.get(),pwdE.get()))
+    
+root = Tk()
+root.title("ch5_6")                     # 視窗標題
+
+msg = "歡迎進入Silicon Stone Educaiton系統"
+sseGif = PhotoImage(file="sse.gif")     # Logo影像檔
+logo = Label(root,image=sseGif,text=msg,compound=BOTTOM)
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=1)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=2)
+
+logo.grid(row=0,column=0,columnspan=2,pady=10,padx=10)
+accountE = Entry(root)                  
+pwdE = Entry(root,show="*")             
+accountE.insert(0,"Kevin")              # 預設Account內容
+pwdE.insert(0,"pwd")                    # 預設pwd內容
+accountE.grid(row=1,column=1)          
+pwdE.grid(row=2,column=1,pady=10)       
+# 以下建立Login和Quit案鈕
+loginbtn = Button(root,text="Login",command=printInfo)
+loginbtn.grid(row=3,column=0,sticky=W,pady=5)      # 切齊
+quitbtn = Button(root,text="Quit",command=root.destroy)
+quitbtn.grid(row=3,column=1,sticky=W,pady=5)      # 切齊
+
+root.mainloop()
+
+
+# %%
+from tkinter import *
+def printInfo():                        # 列印輸入資訊
+    print("Account: %s\nPassword: %s" % (accountE.get(),pwdE.get()))
+    accountE.delete(0,END)              # 刪除account文字方塊的帳號內容
+    pwdE.delete(0,END)                  # 刪除pwd文字方塊的密碼內容
+    
+root = Tk()
+root.title("ch5_7")                     # 視窗標題
+
+msg = "歡迎進入Silicon Stone Educaiton系統"
+sseGif = PhotoImage(file="sse.gif")     # Logo影像檔
+logo = Label(root,image=sseGif,text=msg,compound=BOTTOM)
+accountL = Label(root,text="Account")   # account標籤
+accountL.grid(row=1)
+pwdL = Label(root,text="Password")      # pwd標籤
+pwdL.grid(row=2)
+
+logo.grid(row=0,column=0,columnspan=2,pady=10,padx=10)
+accountE = Entry(root)                  # 文字方塊account
+pwdE = Entry(root,show="*")             # 文字方塊pwd
+accountE.insert(1,"Kevin")              # 預設Account內容
+pwdE.insert(1,"pwd")                    # 預設pwd內容
+accountE.grid(row=1,column=1)           # 定位文字方塊accou
+pwdE.grid(row=2,column=1,pady=10)       # 定位文字方塊pwd
+# 以下建立Login和Quit案鈕
+loginbtn = Button(root,text="Login",command=printInfo)
+loginbtn.grid(row=3,column=0,sticky=W,pady=5)
+quitbtn = Button(root,text="Quit",command=root.destroy)
+quitbtn.grid(row=3,column=1,sticky=W,pady=5)
+
+root.mainloop()
+
+
+
+# %%
+
+from tkinter import *
+
+expression = input("請輸入數學表達式 :")
+print("結果是 : ", eval(expression))    #字串 傳回
+
+# %%
+from tkinter import *
+def cal():                          # 執行數學式計算
+    out.configure(text = "結果 : " + str(eval(equ.get())))
+    
+root = Tk()
+root.title("ch5_9")
+label = Label(root, text="請輸入數學表達式:")
+label.pack()
+equ = Entry(root)                   # 在此輸入表達式
+equ.pack(pady=5)                    
+out = Label(root)                   # 存放計算結果
+out.pack()                          
+btn = Button(root,text="計算",command=cal)    # 計算按鈕
+btn.pack(pady=5)
+
+root.mainloop()
+
+
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+# %%
+
+
+
+
+# %%
 
 
 
