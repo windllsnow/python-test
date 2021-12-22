7d8e2a0d5df7b6deec71a8f3c3d098de958a928f
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2021-12-22 23:42:59 +0800

    Add files via upload

--- 1001.py
+++ 1001.py
@@ -1,3 +1,4 @@
+# %%
 class Animals:
     this_is_a_property = {
         'key 1': 'Value'
@@ -11,7 +12,7 @@ class Animals:
     def this_is_method(self):
         print(self.this_list)
 
-    @property
+    @property  # 只能讀取  不能更改
     def get_gully(self):
         return self.this_list[2]
 
@@ -20,3 +21,29 @@ the_animal = Animals()
 the_animal.this_is_method()
 gully = the_animal.get_gully
 print("The cutest go of all time is", gully)
+
+
+# %%
+
+class Animal:
+    fur_color="Orange"
+    
+    def eat(self):
+        pass
+    def speak(self):
+
+        print("Raaaaaawwwwrrr")
+
+    def chase(self):
+        pass
+
+class Tiger(Animal):
+    def speak(self):
+        print("They're GREEEEEAATTTTTT")
+
+tiger = Tiger()
+tiger.speak()
+
+
+
+
