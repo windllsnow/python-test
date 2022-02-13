e5266e2f361171dcb884487c5947236059ac97d1
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-02-13 23:50:11 +0800

    Add files via upload

--- test002.py
+++ test002.py
@@ -1,8 +1,29 @@
 
 # %%
-x = 10.4548
-print(f'my is  name: {x:.2f}')
-# %%
-print("xyz")
+
+import random
+
+
+def guessing_game():
+    x = random.uniform(0, 100)
+
+    xx1 = round(x)
+
+    test = True
+    while test:
+        y = int(input("猜猜數字(0-99) : "))
+        if (y == xx1):
+            print("猜對了")
+            print(f"your number is {y}")
+            test = False
+        elif(y > xx1):
+            print("猜太高，重來")
+            print(f"your number is {y}")
+        else:
+            print("太低了，重來")
+            print(f"your number is {y}")
+
+
+guessing_game()
 
 # %%
