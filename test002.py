2a5db3a98b35f3b5bf6ccb0c5eebe1b3372a48e6
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-02-14 22:58:32 +0800

    Add files via upload

--- test002.py
+++ test002.py
@@ -1,6 +1,6 @@
 
 # %%
-
+# 01
 import random
 
 
@@ -11,19 +11,28 @@ def guessing_game():
 
     test = True
     while test:
-        y = int(input("猜猜數字(0-99) : "))
-        if (y == xx1):
-            print("猜對了")
-            print(f"your number is {y}")
-            test = False
-        elif(y > xx1):
-            print("猜太高，重來")
-            print(f"your number is {y}")
+
+        y = input("猜猜數字(0-99) : ")
+        if y.isdigit():
+            y1 = int(y)
+            if (y1 >= 0 and y1 <= 99):
+                if (y1 == xx1):
+                    print("猜對了")
+                    print(f"your number is {y1}")
+                    test = False
+                elif(y1 > xx1):
+                    print("猜太高，重來")
+                    print(f"your number is {y1}")
+                else:
+                    print("太低了，重來")
+                    print(f"your number is {y1}")
+            else:
+                print("請重輸： 0-99")
         else:
-            print("太低了，重來")
-            print(f"your number is {y}")
+            print("請重輸: 數字only")
 
 
 guessing_game()
 
 # %%
+# 02
