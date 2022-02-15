2462ec7425227c9181b4c4f774432e8f92bde303
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-02-15 22:32:37 +0800

    Add files via upload

--- test001.py
+++ test001.py
@@ -1,22 +1,25 @@
+# %%
+from pathlib import Path
+import django
 while True:
-    pw=1234
-    if pw ==1234:
+    pw = 1234
+    if pw == 1234:
         pw1 = int(input('請輸入密碼:\t'))
-        if pw!=pw1:
+        if pw != pw1:
             print("密碼錯誤！\n")
-            pw=pw1
+            pw = pw1
         else:
             print("密碼正確！\n")
             break
 
 while True:
     print("\n正確，歡迎光臨！\n")
-    art =int(input('請輸入國文成績: \t'))
+    art = int(input('請輸入國文成績: \t'))
     math = int(input('請輸入數學成績:\t'))
     eng = int(input('請輸入英文成績:\t'))
-    if art<0 or math<0 or eng<0:
+    if art < 0 or math < 0 or eng < 0:
         print("wrong number  重新輸入")
-    elif art>100 or math>100 or eng>100:
+    elif art > 100 or math > 100 or eng > 100:
         print("wrong number  重新輸入")
     else:
         sum = math + eng + art
@@ -24,3 +27,30 @@ while True:
         break
 
 print(f"成績總分:{sum:10d},平均成績:{avg:15.3f}")
+
+
+# %%
+
+squares = []
+for i in range(10):
+    squares.append(i**2)
+
+print(squares)
+
+print([i**2 for i in range(10)])
+# %%
+
+# import django
+django.__version__
+
+# %%
+#from pathlib import Path
+
+Path('spam')/'bacon'/'eggs'
+
+# 一樣
+
+# open(Path('C:\\')/'Users'/'AI'/'Desktop'/'spam.py')
+# open(r'C:\Users\AI\Desktop\spam.py')
+
+Path.home()  # 家目錄
