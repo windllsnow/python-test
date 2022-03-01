27117f7087709222b0c18db25f6e2514bcdb0012
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-03-01 23:38:17 +0800

    Add files via upload

--- 100day.py
+++ 100day.py
@@ -6,6 +6,7 @@
 
 # 集合的 pop()
 
+import re
 from replit import clear
 import numpy as np
 from timeit import default_timer as timer
@@ -1293,6 +1294,35 @@ def format_name(f_name, l_name):
 
 print(format_name(input("your first name?"), input("your last name?")))
 # %%
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print("Leap year")
+                return False
+            else:
+                print("Not leap year")
+                return True
+        else:
+            print("Leap year")
+            return False
+    else:
+        print("Not leap year")
+        return True
+
+def days_in_month(year, month):
+    month_days_01 = [31,28,31,30,31,30,31,31,30,31,30,31]
+    month_days_02 = [31,29,31,30,31,30,31,31,30,31,30,31]
+    if is_leap(year) == True:
+        return month_days_01[month-1]
+    else:
+        return month_days_02[month-1]
+    
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+days = days_in_month(year,month)
+print(days)
 # %%
 # %%
 # %%
