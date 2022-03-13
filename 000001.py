63dca23caf3b0126ea984fe029fe8dd65a287c84
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-03-13 15:02:49 +0800

    Add files via upload

--- 000001.py
+++ 000001.py
@@ -10,26 +10,50 @@ print(a)
 print(123)
 
 
-#%%
+# %%
 
-n = int (input())
+n = int(input())
 a = 0
 
 for i in range(n):
     a = int(input())
-    if a > 5 :
+    if a > 5:
         print(a, ",", end="")
 
 
+# %%
+n = 10
+
+
+bb = []
+
+for i in range(n):
+    bb.append(i)
+    print(bb)
+
+
+print(bb)
+print(type(bb))
+
+# what the fuck 
+# 要先轉 str 再 轉
+
+Str_bb ="".join([str(_)for _ in bb]) 
+    
+print(Str_bb)
+    
+
+# %%
+if __name__ == '__main__':
+    n = int(input())
+    bb = []
+
+    for i in range(1,n+1):
+        bb.append(i)
+    
+    Str_bb ="".join([str(_)for _ in bb]) 
+    
+    print(Str_bb)
 #%%
 
 
-
-
-
-
-
-
-
-
-#%%
