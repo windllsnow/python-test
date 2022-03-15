23ee95da33de82b60d6150b83c7284d8a8d8d107
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-03-15 18:46:31 +0800

    Add files via upload

--- 000002.py
+++ 000002.py
@@ -15,9 +15,19 @@ for value in false_values:
 
 
 
+print((lambda x : x +3 )(3))
+
+employee = {'Alice' : 10000,
+            'Bob' : 99817,
+            'Carol' : 122990,
+            'Frank' : 88123}
 
 
-
+top = []
+for key , val in employee.items():
+    if val >=100000:
+        top.append((key,val))
+print(top)
 
 
 #%%
