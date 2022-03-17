8b685b8bf4b558acc4223496761e2c872a79d7c6
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-03-17 23:53:57 +0800

    Add files via upload

--- 000001.py
+++ 000001.py
@@ -55,5 +55,33 @@ if __name__ == '__main__':
     
     print(Str_bb)
 #%%
+'''
+List.extend(iterable) 將每個 iterator 加入 list
+'''
+xx = list(range(5))
+print(xx)
+yy = list(range(2))
+print(yy)
+xx.extend(yy)
+print(xx)
+
+'''
+List.append(object) 將 object 加入 list
+'''
+xx = list(range(5))
+print(xx)
+yy = list(range(2))
+print(yy)
+
+xx.append(yy)
+print(xx)
+'''
+從上面兩個例子可知道，
+extend 是取出 object 的所有 element/iterator 扔進list 裡面，
+而 append 則直接把 object 塞進 list 裡面，
+不管 object 是哪種類型的資料結構。
+'''
+#%%
 
 
+#%%
