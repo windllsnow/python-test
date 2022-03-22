308edd6b36a3038a37e100b1ab76fbd3566cf578
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-03-22 21:08:12 +0800

    Add files via upload

--- 000002.py
+++ 000002.py
@@ -16,7 +16,7 @@ for value in false_values:
 
 
 print((lambda x : x +3 )(3))
-
+#%%
 employee = {'Alice' : 10000,
             'Bob' : 99817,
             'Carol' : 122990,
@@ -28,6 +28,22 @@ for key , val in employee.items():
     if val >=100000:
         top.append((key,val))
 print(top)
+
+
+print("\n")
+
+top1 = [(k,v) for k, v in employee.items() if v >=100000] 
+print(top1)
+#%%
+
+
+
+#[表達式 + 條件式]
+[x * 2 for x in range(5)]
+print([(x,y) for x in range(5) for y in range(3)])
+print([x **2  for x  in range(10) if x % 2 >0 ])
+print([x.lower() for x in ['I','AM','NOT','SHOUTING']])
+
 
 
 #%%
