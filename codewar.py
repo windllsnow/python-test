5ee10af94492d23a0313ba022c81c6e9c7146d77
wind_flowing <87889501+windllsnow@users.noreply.github.com>
2022-09-01 21:28:13 +0800

    Add files via upload

--- codewar.py
+++ codewar.py
@@ -11,17 +11,11 @@ def problem(a):
     if type(a) == str:
         return "Error"
     else:
-
         answer = a*50+6
         return answer
-
-
 #
-
 problem = lambda a: 50*a+6 if isinstance(a, (int, float))  else "Error" 
-
 #
-
 # chr str
 #   codewar 不用 input 直接打在 def  
 # attempt-->test
@@ -30,5 +24,21 @@ problem = lambda a: 50*a+6 if isinstance(a, (int, float))  else "Error"
 
 #%% 
 
+def  square_sum(numbers):
+    def square(numbers):
+        return map(lambda x: x**2, numbers)
+    y= square(numbers)
+    Sum= sum(y)
+    return print(Sum)
+
+square_sum([1, 2, 3, 4, 5])
 
 #%%
+
+
+
+
+#%% 
+
+
+
